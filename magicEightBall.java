4bf5a8886da04bc530b5a5fdb808201c812901e0
Matthew <mfarmer5102@gmail.com>
2019-09-05 12:10:36 -0400

    Able to test index of question mark in question provided by the user

--- magicEightBall.java
+++ magicEightBall.java
@@ -1,13 +1,34 @@
 import java.util.Random;
+import java.util.Scanner;
 
-public class MyClass {
+public class Global {
 
-    static void findIndexOf(String context, String character) {
-        int location = context.indexOf(character);
+    // GLOBAL VARIABLES ###########################################################
+
+    public static String questionAskedByUser = "Will I win the lottery?";
+
+    // FUNCTIONS ##################################################################
+
+    // TAKE IN A QUESTION FROM THE USER
+    static void gatherAndStoreInput() {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("I'm a magic eight ball. Ask me a question!");
+
+        // String input
+        String name = myObj.nextLine();
+        Global.questionAskedByUser = name;
+    }
+
+    // VERIFY A QUESTION MARK APPEARS IN THE STRING
+    static void testIfQuestion(String context) {
+        int location = context.indexOf("?");
         System.out.println(location);
     }
 
+    // RUN PROGRAM #################################################################
+
     public static void main(String[] args) {
-        findIndexOf("Banana", "x");
+        gatherAndStoreInput();
+        testIfQuestion(Global.questionAskedByUser);
     }
 }
